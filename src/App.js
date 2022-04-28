81eb39ba2945c775b9794706c4be200b45130024
TFVO11 <cosmoyj@naver.com>
2022-04-29 01:33:23 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //import ExpenseItem from './components/ExpenseItem.js';
 import Expense from './components/Expenses/Expense.js';
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,20 +2,30 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+    
+    //function clickHandler() {}
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
 
     return (
         <Card className='expense-item'>
 
             <ExpenseDate date={props.date} />
             <div className='expense-item__descriptio'>
-                <h1>{props.title}</h1>
+                <h1>{title}</h1>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
-
+//button onClick={clickHandler}는 클릭핸들러라는 함수를 
+//실행한다는 것이다. 
     );
 }
 
